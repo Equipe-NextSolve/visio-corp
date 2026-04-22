5327c4ef74f05333091f97b4fcbd8f25b2e222a4
Guilherme <guilhermebarroso166@gmail.com>
2026-04-22 13:50:57 -0300

    criação do sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,18 @@
+export default function sitemap() {
+  const baseUrl = 'https:/www.visiocorp.com.br/'
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+
+  ]
+}
